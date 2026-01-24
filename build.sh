dc9be8d262e772846903231fb79810a8c72bc2c3
Yanglin Zhao <hi@yanglinzhao.com>
2026-01-24 14:07:25 -0500

    copy source

--- build.sh
+++ build.sh
@@ -4,6 +4,16 @@ DIR=$(dirname "$(realpath $0)")
 export QMK_USERSPACE="${DIR}/out"
 export SKIP_FLASHING_SUPPORT=1
 
-cd qmk_firmware
-util/docker_build.sh silakka54:default
-cd -
+function copy_source() {
+  rm -rf qmk_firmware/keyboards/silakka54
+  cp -r silakka54 qmk_firmware/keyboards/
+}
+
+function compile() {
+  cd qmk_firmware
+  util/docker_build.sh silakka54:default
+  cd -
+}
+
+copy_source
+compile
