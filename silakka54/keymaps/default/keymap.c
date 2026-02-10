b2a50b323c2d68f658ca233868b082593c0bf4cd
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-09 21:16:35 -0500

    swap caps and nums

--- silakka54/keymaps/default/keymap.c
+++ silakka54/keymaps/default/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,        KC_T,                             KC_Y,  KC_U,        KC_I,    KC_O,   KC_P,      KC_BSPC,
         KC_ESC,   HOME_A, HOME_S, HOME_D, HOME_F,      KC_G,                             KC_H,  HOME_J,      HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,        KC_B,                             KC_N,  KC_M,        KC_COMM, KC_DOT, KC_SLSH,   TD(TD_CAPS),
-                                          TD(TD_CAPS), LT(2, KC_TAB), KC_SPC,    KC_ENT, MO(1), TD(TD_NUMS)
+                                          TD(TD_NUMS), LT(2, KC_TAB), KC_SPC,    KC_ENT, MO(1), TD(TD_CAPS)
     ),
 
     // Symbol Layer
