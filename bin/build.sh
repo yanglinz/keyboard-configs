c64620476fda3d4920e33be0e3e2dc636454da57
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-24 21:06:20 -0500

    Add kyria configs (#10)
    * Add kyria build info
    
    * almost being able to compile kyria
    
    * fix build

--- bin/build.sh
+++ bin/build.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "$(realpath $0)")
-export QMK_USERSPACE="${DIR}/out"
+export QMK_USERSPACE="${DIR}/../out"
 export SKIP_FLASHING_SUPPORT=1
 
 function copy_source() {
   rm -rf qmk_firmware/keyboards/silakka54
   cp -r silakka54 qmk_firmware/keyboards/
+
+  rm -rf qmk_firmware/keyboards/splitkb/halcyon/kyria
+  cp -r kyria qmk_firmware/keyboards/splitkb/halcyon/
 }
 
 function compile() {
   cd qmk_firmware
-  util/docker_build.sh silakka54:default
+  util/docker_build.sh splitkb/halcyon/kyria/rev4:default
+  # TODO: add logic to compare diff against master
+  # util/docker_build.sh silakka54:default
   cd -
 }
 
