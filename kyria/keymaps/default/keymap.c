ba067c51a83123c768c5b11602754b3035d6aa8c
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-27 14:29:25 -0500

    port tap dance

--- kyria/keymaps/default/keymap.c
+++ kyria/keymaps/default/keymap.c
@@ -8,8 +8,24 @@ enum layers {
     _ADJUST,
 };
 
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
+
+
 // todo:
 // 1. port base layer
+// -. port hmr
+// -. port tap dance
 // 2. port symbol layer
 // 3. port nav layer
 // 4. implement image gen
