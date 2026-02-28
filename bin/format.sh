8a38fd3fd3778f71891e5561f3c0c5fee1b0e7d1
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 14:29:04 -0500

    format files

--- bin/format.sh
+++ bin/format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-clang-format -i kyria/**/*.c
-clang-format -i silakka54/**/*.c
+clang-format -i ./kyria/**/*.c
+clang-format -i ./silakka54/**/*.c
--- kyria/rev4/rev4.c
+++ kyria/rev4/rev4.c
@@ -5,24 +5,24 @@
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) {
-        return false;
-    }
-    if (index == 0) {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) {
-        // Page up/Page down
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
+  if (!encoder_update_user(index, clockwise)) {
     return false;
+  }
+  if (index == 0) {
+    // Volume control
+    if (clockwise) {
+      tap_code(KC_VOLU);
+    } else {
+      tap_code(KC_VOLD);
+    }
+  } else if (index == 1) {
+    // Page up/Page down
+    if (clockwise) {
+      tap_code(KC_PGDN);
+    } else {
+      tap_code(KC_PGUP);
+    }
+  }
+  return false;
 }
 #endif
