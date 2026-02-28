9b9503a7932ccf4acace10a279a064350d7268b0
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 14:21:55 -0500

    Create new layout for kyria (#12)
    * delete and start a new
    
    * base state passing build
    
    * port tap dance
    
    * define hrm
    
    * implement base layer
    
    * implement tap dance
    
    * more progress
    
    * fix build
    
    * implement symbol layer
    
    * implement nav layer
    
    * implement all layers
    
    * fix windows vs macos layout
    
    * fix
    
    * Update keymap.c

--- kyria/rev4/config.h
+++ kyria/rev4/config.h
@@ -3,6 +3,14 @@
 
 #pragma once
 
+// home row mods (timerless)
+// https://www.reddit.com/r/ErgoMechKeyboards/comments/1q1jo3c/urobs_zmk_timeless_home_row_mods_ported_to_native/
+#define TAPPING_TERM 270
+#define PERMISSIVE_HOLD
+#define FLOW_TAP_TERM 130
+#define CHORDAL_HOLD
+#define SPECULATIVE_HOLD
+
 // Make it easier to enter the bootloader
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
