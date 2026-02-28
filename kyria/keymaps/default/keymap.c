4cd26bc7b06842ce648676debe6114b9dcbaafc6
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-27 21:18:25 -0500

    fix build

--- kyria/keymaps/default/keymap.c
+++ kyria/keymaps/default/keymap.c
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
     // Base qwerty layer
-    KC_TAB,  KC_Q, KC_W, KC_E, KC_R,     KC_T                                                         KC_Y,        KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC,
-    KC_ESC,  HR_A, HR_S, HR_D, HR_F,     KC_G                                                         KC_H,        HR_J,  HR_K,    HR_L,   HR_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V,     KC_B,  KC_NO,      KC_NO,                  KC_NO,  KC_NO,    KC_N,        KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                               MO(_ADJ), KC_NO, TD(TD_NUM), MO(_NAV), KC_SPC        KC_ENT, MO(_SYM), TD(TD_CAPS), KC_NO, KC_NO
+    KC_TAB,  KC_Q, KC_W, KC_E, KC_R,     KC_T,                                                     KC_Y,        KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC,
+    KC_ESC,  HR_A, HR_S, HR_D, HR_F,     KC_G,                                                     KC_H,        HR_J,  HR_K,    HR_L,   HR_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V,     KC_B,  KC_NO,      KC_NO,               KC_NO,  KC_NO,    KC_N,        KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                               MO(_ADJ), KC_NO, TD(TD_NUM), MO(_NAV), KC_SPC,    KC_ENT, MO(_SYM), TD(TD_CAPS), KC_NO, KC_NO
     ),
 
     /*
