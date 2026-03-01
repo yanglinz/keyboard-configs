be5d6efb75aa282495268d053e649939ebc560bf
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 21:39:26 -0500

    more progress

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-./bin/qmk-cli.sh c2json keyboards/splitkb/halcyon/kyria/keymaps/default/keymap.c > ./keymap-drawings/kyria.json
-jq '.keyboard = "splitkb/halcyon/kyria/rev4"' ./keymap-drawings/kyria.json > ./keymap-drawings/kyria.json.tmp && mv ./keymap-drawings/kyria.json.tmp ./keymap-drawings/kyria.json
+./bin/qmk-cli.sh c2json -kb splitkb/halcyon/kyria/rev4 -km default > ./keymap-drawings/kyria.json
 uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/kyria.json > ./keymap-drawings/kyria.yaml
 uvx --from keymap-drawer keymap draw ./keymap-drawings/kyria.yaml > ./keymap-drawings/kyria.svg
 
