3ce9826d32a9e201de0c6aea29e830689613cc71
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 21:14:52 -0500

    progress

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c
-# uvx --from keymap-drawer keymap parse -c 14 -q ./silakka54/keyboard.json > ./image/silakka54.yml
+./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c > ./keymap-drawings/silakka54.json
+
+# uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/silakka54.json
 # uvx --from keymap-drawer keymap draw ./image/lotus58.yml > image/lotus58.svg
