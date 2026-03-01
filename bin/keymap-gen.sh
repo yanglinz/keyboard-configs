14c1f5273e50fa4e9963d5302368f9aae4f33b80
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 21:19:38 -0500

    keymap drawings

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c > ./keymap-drawings/silakka54.json
-
-# uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/silakka54.json
-# uvx --from keymap-drawer keymap draw ./image/lotus58.yml > image/lotus58.svg
+./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c > ./keymap-drawings/silakka.json
+uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/silakka.json > ./keymap-drawings/silakka.yaml
+uvx --from keymap-drawer keymap draw ./keymap-drawings/silakka.yaml > ./keymap-drawings/silakka.svg
