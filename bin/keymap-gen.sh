240d10dc8658ea7614c440abed5918ddab435e4f
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 21:55:22 -0500

    add script

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# ./bin/qmk-cli.sh c2json -kb splitkb/halcyon/kyria/rev4 -km default > ./keymap-drawings/kyria.json
-# uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/kyria.json > ./keymap-drawings/kyria.yaml
+./bin/qmk-cli.sh c2json -kb splitkb/halcyon/kyria/rev4 -km default > ./keymap-drawings/kyria.json
+uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/kyria.json > ./keymap-drawings/kyria.yaml
+uv run ./keymap-drawings/kyria.py
 uvx --from keymap-drawer keymap draw ./keymap-drawings/kyria.yaml > ./keymap-drawings/kyria.svg
 
 ./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c > ./keymap-drawings/silakka.json
