6ab8561d76b202dd87c97e824d45c7fdc81144c3
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 21:08:34 -0500

    Create helper script for running

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-uvx --from keymap-drawer keymap parse -c 14 -q ./silakka54/keyboard.json > ./image/silakka54.yml
+./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c
+# uvx --from keymap-drawer keymap parse -c 14 -q ./silakka54/keyboard.json > ./image/silakka54.yml
 # uvx --from keymap-drawer keymap draw ./image/lotus58.yml > image/lotus58.svg
---
+++ bin/qmk-cli.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$(dirname "$(realpath $0)")/.."
+
+docker run --rm -it \
+  -w /qmk_firmware \
+  -v "${ROOT_DIR}/qmk_firmware:/qmk_firmware:z" \
+  -v "${ROOT_DIR}/out:/qmk_userspace:z" \
+  -e QMK_USERSPACE=/qmk_userspace \
+  -e SKIP_GIT= -e SKIP_VERSION= -e MAKEFLAGS= \
+  ghcr.io/qmk/qmk_cli qmk "$@"
