e56c7cbf2099d3a70be894962db925d1eec95a83
Yanglin Zhao <hi@yanglinzhao.com>
2026-02-28 21:22:42 -0500

    try to generate kyria too

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+./bin/qmk-cli.sh c2json keyboards/splitkb/halcyon/kyria/keymaps/default/keymap.c > ./keymap-drawings/kyria.json
+uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/kyria.json > ./keymap-drawings/kyria.yaml
+uvx --from keymap-drawer keymap draw ./keymap-drawings/kyria.yaml > ./keymap-drawings/kyria.svg
+
 ./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c > ./keymap-drawings/silakka.json
 uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/silakka.json > ./keymap-drawings/silakka.yaml
 uvx --from keymap-drawer keymap draw ./keymap-drawings/silakka.yaml > ./keymap-drawings/silakka.svg
