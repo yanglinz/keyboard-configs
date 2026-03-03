30565a1123a4935f30ee30ce6c1a69e67280dacc
Yanglin Zhao <hi@yanglinzhao.com>
2026-03-02 22:34:50 -0500

    More kyria tweaks (#16)
    * tweaks
    
    * move todos
    
    * implement fix
    
    * add todo comment
    
    * try reset
    
    * just don't use one shot
    
    * more changes
    
    * fix layer
    
    * enable lighting
    
    * lighting
    
    * lighting
    
    * add todo

--- kyria/keymaps/default/keymap.c
+++ kyria/keymaps/default/keymap.c
@@ -1,5 +1,10 @@
 #include QMK_KEYBOARD_H
 
+// todo:
+// - implement macos vs windows
+// https://github.com/mmccoyd/hillside/tree/main/hillside46
+// https://www.reddit.com/r/ErgoMechKeyboards/comments/1c9satw/whats_your_favorite_productivity_macro/
+
 enum layers {
   _BASE = 0,
   _SYM,
@@ -30,13 +35,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   );
 // clang-format on
 
-// todo:
-// implement image gen post processing
-// implement layer reset button
-// layer led
-// implement macos vs windows
-// https://www.reddit.com/r/ErgoMechKeyboards/comments/1c9satw/whats_your_favorite_productivity_macro/
-
+// Tap dance logic
 enum tdance {
   TD_CAPS = 0,
   TD_NUM,
@@ -45,7 +44,7 @@ enum tdance {
 void td_caps_finished(tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     set_oneshot_mods(MOD_LSFT);
-  } else if (state->count >= 2) {
+  } else if (state->count == 2) {
     caps_word_toggle();
   }
 }
@@ -53,7 +52,7 @@ void td_caps_finished(tap_dance_state_t *state, void *user_data) {
 void td_nums_finished(tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     layer_invert(_NAV);
-  } else if (state->count >= 2) {
+  } else if (state->count == 2) {
     layer_invert(_NUM);
   }
 }
@@ -61,6 +60,42 @@ void td_nums_finished(tap_dance_state_t *state, void *user_data) {
 tap_dance_action_t tap_dance_actions[] = {
     [TD_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_caps_finished, NULL),
     [TD_NUM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_nums_finished, NULL)};
+
+// LED lighting
+bool rgb_matrix_indicators_user(void) {
+  uint8_t layer = get_highest_layer(layer_state);
+  // todo get tap dance state and represent that in the led
+
+  switch (layer) {
+  case _BASE:
+    rgb_matrix_sethsv(0, 0, 0);
+    rgb_matrix_set_color_all(0, 0, 0); // off
+    break;
+  case _SYM:
+    rgb_matrix_sethsv(0, 255, 255);
+    rgb_matrix_set_color_all(0, 255, 0); // green
+    break;
+  case _NAV:
+    rgb_matrix_sethsv(0, 255, 255);
+    rgb_matrix_set_color_all(0, 100, 255); // blue
+    break;
+  case _NUM:
+    rgb_matrix_sethsv(0, 255, 255);
+    rgb_matrix_set_color_all(255, 100, 0); // orange
+    break;
+  case _ADJ:
+    rgb_matrix_sethsv(0, 255, 255);
+    rgb_matrix_set_color_all(255, 0, 0); // red
+    break;
+  default:
+    rgb_matrix_sethsv(0, 255, 255);
+    rgb_matrix_set_color_all(255, 0, 0); // red
+    break;
+  }
+
+  // Indicate that we've handled the frame
+  return true;
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -70,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q, KC_W, KC_E,     KC_R,  KC_T,                                                 KC_Y,     KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC,
     KC_ESC,  HR_A, HR_S, HR_D,     HR_F,  KC_G,                                                 KC_H,     HR_J,  HR_K,    HR_L,   HR_SCLN, KC_QUOT,
     KC_LSFT, KC_Z, KC_X, KC_C,     KC_V,  KC_B,     KC_NO,  KC_NO,         KC_NO,       KC_NO,  KC_N,     KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                         MO(_ADJ), KC_NO, MO(_NAV), KC_SPC, TD(TD_NUM),    TD(TD_CAPS), KC_ENT, MO(_SYM), KC_NO, KC_NO
+                         MO(_ADJ), KC_NO, MO(_NAV), KC_SPC, TD(TD_NUM),    TD(TD_CAPS), KC_ENT, MO(_SYM), KC_NO, TO(_BASE)
   ),
 
   // Symbol layer
@@ -83,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Navigation layer
   [_NAV] = LAYOUT(
-    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_DEL,
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_BSPC,
     KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_NO,                                          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_CAPS, KC_TRNS,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_INS,  KC_TRNS,
                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
@@ -91,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Number pad layer
   [_NUM] = LAYOUT(
-    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9, KC_0,  KC_DEL,
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9, KC_0,  KC_BSPC,
     KC_TRNS, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_NO,                                          KC_0,    KC_4,    KC_5,    KC_6, KC_NO, KC_TRNS,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_0,    KC_1,    KC_2,    KC_3, KC_NO, KC_TRNS,
                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
