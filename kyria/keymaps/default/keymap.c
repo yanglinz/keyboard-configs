9282dc5ce0a7e3d30bd6297729f83033ec19085b
Yanglin Zhao <hi@yanglinzhao.com>
2026-03-03 20:55:54 -0500

    Implement macos and os specific layers (#18)
    * add persistent base layers
    
    * enable combos
    
    * better combo

--- kyria/keymaps/default/keymap.c
+++ kyria/keymaps/default/keymap.c
@@ -1,29 +1,43 @@
 #include QMK_KEYBOARD_H
 
-// todo:
-// - implement macos vs windows
-// https://github.com/mmccoyd/hillside/tree/main/hillside46
-// https://www.reddit.com/r/ErgoMechKeyboards/comments/1c9satw/whats_your_favorite_productivity_macro/
-
 enum layers {
   _BASE = 0,
+  _WIN,
   _SYM,
   _NAV,
   _NUM,
   _ADJ,
 };
 
-// Left-hand home row mods
+/*
+ * =============
+ * Home row mods
+ * =============
+ */
+
+// Left-hand home row mods (MacOS)
 #define HR_A LCTL_T(KC_A)
 #define HR_S LALT_T(KC_S)
 #define HR_D LGUI_T(KC_D)
 #define HR_F LSFT_T(KC_F)
 
-// Right-hand home row mods
+// Right-hand home row mods (MacOS)
 #define HR_J RSFT_T(KC_J)
 #define HR_K RGUI_T(KC_K)
 #define HR_L LALT_T(KC_L)
 #define HR_SCLN RCTL_T(KC_SCLN)
+
+// Left-hand home row mods (Windows)
+#define HRW_A LGUI_T(KC_A)
+#define HRW_S LALT_T(KC_S)
+#define HRW_D LCTL_T(KC_D)
+#define HRW_F LSFT_T(KC_F)
+
+// Right-hand home row mods (Windows)
+#define HRW_J RSFT_T(KC_J)
+#define HRW_K RCTL_T(KC_K)
+#define HRW_L LALT_T(KC_L)
+#define HRW_SCLN RGUI_T(KC_SCLN)
 
 // clang-format off
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
@@ -35,7 +49,12 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   );
 // clang-format on
 
-// Tap dance logic
+/*
+ * =========
+ * Tap dance
+ * =========
+ */
+
 enum tdance {
   TD_CAPS = 0,
   TD_NUM,
@@ -61,7 +80,43 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_caps_finished, NULL),
     [TD_NUM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_nums_finished, NULL)};
 
-// LED lighting
+/*
+ * =========
+ * Macros
+ * =========
+ */
+
+enum custom_keycodes {
+  MC_SEL_LINE = SAFE_RANGE,
+};
+
+const uint16_t PROGMEM line_combo[] = {KC_X, KC_DOT, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(line_combo, MC_SEL_LINE),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case MC_SEL_LINE:
+    if (record->event.pressed) {
+      // https://www.reddit.com/r/ErgoMechKeyboards/comments/1c9satw/whats_your_favorite_productivity_macro/
+      tap_code(KC_HOME);
+      register_code(KC_LSFT);
+      tap_code(KC_END);
+      unregister_code(KC_LSFT);
+    }
+    return false;
+  }
+  return true;
+}
+
+/*
+ * =========
+ * Tap dance
+ * =========
+ */
+
 bool rgb_matrix_indicators_user(void) {
   uint8_t layer = get_highest_layer(layer_state);
   // todo: get tap dance state and represent that in the led
@@ -76,6 +131,7 @@ bool rgb_matrix_indicators_user(void) {
     rgb_matrix_set_color_all(255, 0, 0); // red
     break;
   case _BASE:
+  case _WIN:
   case _SYM:
   case _NAV:
   default:
@@ -99,7 +155,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          MO(_ADJ), KC_NO, MO(_NAV), KC_SPC, TD(TD_NUM),    TD(TD_CAPS), KC_ENT, MO(_SYM), KC_NO, TO(_BASE)
   ),
 
+  [_WIN] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_TRNS, HRW_A,   HRW_S,   HRW_D,   HRW_F,   KC_TRNS,                                     KC_TRNS, HRW_J,   HRW_K,   HRW_L,   HRW_SCLN, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
   // Symbol layer
+  // todo: migrate some over from https://github.com/mmccoyd/hillside/tree/main/hillside46
   [_SYM] = LAYOUT(
     KC_TRNS, KC_GRV,     LSFT(KC_GRV),   KC_HASH,    LSFT(KC_7),     KC_PIPE,                                          LSFT(KC_6), LSFT(KC_LBRC), LSFT(KC_RBRC),  KC_LBRC,        KC_RBRC,        KC_DEL,
     KC_TRNS, LSFT(KC_1), LSFT(KC_MINUS), KC_COLON,   KC_EQUAL,       KC_DOLLAR,                                        LSFT(KC_2), LSFT(KC_9),    LSFT(KC_0),     LSFT(KC_MINUS), KC_SCLN,        KC_TRNS,
@@ -125,9 +189,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Adjustment layer
   [_ADJ] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                                    _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                                    RM_TOGG, RM_SATU, RM_HUEU, RM_VALU, RM_NEXT, _______,
-    _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, RM_SATD, RM_HUED, RM_VALD, RM_PREV, _______,
-                               _______, _______, _______,_______, _______, _______, _______, _______, _______, _______
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     PDF(_BASE), PDF(_WIN), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     RM_TOGG,    RM_SATU,   RM_HUEU, RM_VALU, RM_NEXT, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    RM_SATD,   RM_HUED, RM_VALD, RM_PREV, KC_TRNS,
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS
   ),
 };
