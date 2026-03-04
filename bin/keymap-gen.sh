e479a8e9fab87f1a73d9fdee575221bc3d1058c2
Yanglin Zhao <hi@yanglinzhao.com>
2026-03-03 21:02:47 -0500

    Images are still needed

--- bin/keymap-gen.sh
+++ bin/keymap-gen.sh
@@ -8,6 +8,3 @@ uvx --from keymap-drawer keymap -c ./keymap-drawings/keymap-drawer.config.yaml d
 ./bin/qmk-cli.sh c2json keyboards/silakka54/keymaps/default/keymap.c > ./keymap-drawings/silakka.json
 uvx --from keymap-drawer keymap parse -c 14 -q ./keymap-drawings/silakka.json > ./keymap-drawings/silakka.yaml
 uvx --from keymap-drawer keymap -c ./keymap-drawings/keymap-drawer.config.yaml draw ./keymap-drawings/silakka.yaml > ./keymap-drawings/silakka.svg
-
-uvx --from keymap-drawer keymap parse -c 14 -z ./lotus58/config/lotus58.keymap > ./keymap-drawings/lotus58.yml
-uvx --from keymap-drawer keymap draw ./keymap-drawings/lotus58.yml > ./keymap-drawings/lotus58.svg
