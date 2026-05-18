e3fb0ff39a7d9602f3153e3f18c2b7fe3bff4bb4
Yanglin Zhao <hi@yanglinzhao.com>
2026-05-17 20:57:49 -0400

    Migrate windows layer

--- silakka54/keymaps/default/keymap.c
+++ silakka54/keymaps/default/keymap.c
@@ -86,13 +86,22 @@ tap_dance_action_t tap_dance_actions[] = {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // Base Layer
+
+  // Base qwerty layer
   [_BASE] = LAYOUT(
     MO(_ADJ), KC_1, KC_2, KC_3, KC_4,        KC_5,                       KC_6,     KC_7, KC_8,    KC_9,   KC_0,    KC_EQUAL,
     KC_TAB,   KC_Q, KC_W, KC_E, KC_R,        KC_T,                       KC_Y,     KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
     KC_ESC,   HR_A, HR_S, HR_D, HR_F,        KC_G,                       KC_H,     HR_J, HR_K,    HR_L,   HR_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z, KC_X, KC_C, KC_V,        KC_B,                       KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_CAPS),
                                 TD(TD_NUMS), MO(_NAV), KC_SPC,   KC_ENT, MO(_SYM), KC_NO
+  ),
+
+  [_WIN] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_TRNS, HRW_A,   HRW_S,   HRW_D,   HRW_F,   KC_TRNS,                     KC_TRNS, HRW_J,   HRW_K,   HRW_L,   HRW_SCLN, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+                                        KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   // Symbol Layer
